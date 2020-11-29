a1409b2e9cf4f4b409a7f6c816f743373adefe20
waelnaseeb <66029393+waelnaseeb@users.noreply.github.com>
2020-11-29 22:07:46 +0100

    Update Data_Science II_Final_project_ReDI_school_Wael.py

--- Data_Science II_Final_project_ReDI_school_Wael.py
+++ Data_Science II_Final_project_ReDI_school_Wael.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Implementing a Modern Geophysical Well Log Data Analysis and Visualisation Techniques for Accurate Gap Prediction, Interpretation and Intervention to Eliminate Catastrophes
+# # Implementing Modern Geophysical Well Log Data Analysis and Visualisation Techniques for Accurate Gap Prediction, Interpretation and Intervention to Eliminate Catastrophes
 
 # # Abstract 
 # Well log data collected in the field often include gaps and out of range readings , which means that data have missing values and outliers for many reasons, for instance due to noise. This makes it hard to rely on it for the interpretation of lithological properties. To address the gaps and out of range data issue, I present ansd compare different gap-fill algorithms. The proposed methods predict missing values of each measurement separately based on data points around the missing data point from the same well. The algorithms were applied on a LAS data file from BAUMAN #3 in Kansas.The data used in this work is a free dataset provided by the University of Kansas. In this paper I will be using Python 3 version for coding.
